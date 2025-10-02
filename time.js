4184940b55147c886a1ba15d66da6fa2bf681f63
Hisham <linkedin.hisham@gmail.com>
2025-10-03 00:14:54 +0530

    added wake lock and 12 am being 0

--- time.js
+++ time.js
@@ -1,29 +1,20 @@
 
-
-
 const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const ampm = document.getElementById("ampm");
 
 
-
+updateTime();
 function updateTime() {
     const now = new Date();
     let nowHour = now.getHours();
     let nowMin = now.getMinutes();
     let nowSec = now.getSeconds();
-    let nowAmPm = "AM";
-    if (nowHour == 0) {
-        /* 12 am */
-        nowHour = 12;
-    }
-    if (nowHour >= 12) {
-        nowHour = nowHour - 12;
-        nowAmPm = "PM";
-    } else {
-        nowAmPm = "AM";
-    }
+    let nowAmPm = nowHour >= 12 ? "PM" : "AM";
+    nowHour = nowHour % 12 || 12;
+
+
     nowHour = String(nowHour).padStart(2, "0");
     nowMin = String(nowMin).padStart(2, "0");
     nowSec = String(nowSec).padStart(2, "0");
@@ -34,5 +25,27 @@ function updateTime() {
     seconds.textContent = nowSec;
     ampm.textContent = nowAmPm;
 }
+/* to update time every second */
 setInterval(updateTime, 1000);
-updateTime();
+
+
+/* to setup wake lock */
+let wakelock = null;
+
+async function requestWakeLock() {
+    try {
+        wakelock = await navigator.wakeLock.request("screen");
+        console.log("wake lock is active now ");
+
+    } catch (e) {
+        console.error(`${e.name} ,   ${e.message}`);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", requestWakeLock);
+
+document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "visible") {
+        requestWakeLock();
+    }
+})
